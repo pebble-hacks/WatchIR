584538a90f30319cd5535fe29badf3f0e9b2a708
Kevin Conley <kevinc@getpebble.com>
2015-08-28 00:25:16 -0700

    Added instruction text layer.

--- watchIR_pebble/src/main.c
+++ watchIR_pebble/src/main.c
@@ -2,6 +2,7 @@
 
 typedef struct {
   Window *window;
+  TextLayer *instruction_text_layer;
 } WatchIRAppData;
 
 static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -22,15 +23,28 @@ static void prv_click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
 }
 
+static void prv_window_appear(Window *window) {
+  WatchIRAppData *data = window_get_user_data(window);
+  text_layer_set_text(data->instruction_text_layer, "Hold down a button to program");
+}
+
 static void prv_window_load(Window *window) {
-  // TODO
-//  Layer *window_layer = window_get_root_layer(window);
-//  GRect bounds = layer_get_bounds(window_layer);
+  WatchIRAppData *data = window_get_user_data(window);
+
+  Layer *window_layer = window_get_root_layer(window);
+  const GRect window_layer_bounds = layer_get_bounds(window_layer);
+
+  data->instruction_text_layer = text_layer_create(window_layer_bounds);
+  TextLayer *instruction_text_layer = data->instruction_text_layer;
+  text_layer_set_font(instruction_text_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
+  text_layer_set_text_alignment(instruction_text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(instruction_text_layer));
 }
 
 static void prv_window_unload(Window *window) {
   WatchIRAppData *data = window_get_user_data(window);
   if (data) {
+    text_layer_destroy(data->instruction_text_layer);
     window_destroy(data->window);
   }
   free(data);
@@ -45,6 +59,7 @@ static void prv_app_init(void) {
   window_set_user_data(window, data);
   window_set_click_config_provider(window, prv_click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
+    .appear = prv_window_appear,
     .load = prv_window_load,
     .unload = prv_window_unload,
   });
